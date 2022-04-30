c72247bcd643515c7ba1be5129a8d0b784b19267
lukablaskovic <lblaskovi@unipu.hr>
2022-04-30 15:19:44 +0200

    upoznavanje sa backendom

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require("express");
-console.log(express);
+
+const app = express();
+
+//Routes
+app.get("/auth", async (req, res) => {
+  console.log(req.query);
+  res.send(`Hello ${req.query.userId} It is ${Date.now()}!`);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
