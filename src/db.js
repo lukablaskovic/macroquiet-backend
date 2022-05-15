f02616b4008f2af1b9c2763f3c334d59e19fdebd
lukablaskovic <lblaskovi@unipu.hr>
2022-05-15 12:03:46 +0200

    mongoDB povezivanje i citanje podataka

--- src/db.js
+++ src/db.js
@@ -1,36 +1,29 @@
-import mongo from "mongodb";
+import mongodb from "mongodb";
 
 let connection_string =
   "mongodb+srv://admin:6VKpRNUZ2cvEY9xK@cluster0.bhxeo.mongodb.net/stranded-away";
-let client = new mongo.MongoClient(connection_string, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-client.connect((err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log("Database connected successfully!");
-  // za sada ništa nećemo raditi, samo zatvaramo pristup sljedećom naredbom
-  client.close();
-});
+
+// Create a new MongoClient
+const client = new mongodb.MongoClient(connection_string);
 let db = null;
+// eksportamo Promise koji resolva na konekciju
 export default () => {
   return new Promise((resolve, reject) => {
-    //ako smo inicijalizirali bazu i klijent je još uvijek spojen
+    // ako smo inicijalizirali bazu i klijent je još uvijek spojen
+    /*
     if (db && client.isConnected()) {
       resolve(db);
     } else {
-      client.connect((err) => {
-        if (err) {
-          reject("Spajanje na bazu nije uspjelo:" + err);
-        } else {
-          console.log("Database connected successfully!");
-          db = client.db("stranded-away");
-          resolve(db);
-        }
-      });
-    }
+      */
+    client.connect((err) => {
+      if (err) {
+        reject("Spajanje na bazu nije uspjelo:" + err);
+      } else {
+        console.log("Database connected successfully!");
+        db = client.db("stranded-away");
+        resolve(db);
+      }
+    });
+    //}
   });
 };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import express from "express";
 import storage from "./memory_storage.js";
+import connect from "./db.js";
 import cors from "cors";
 
 const app = express();
 
 app.use(cors()); //Omoguci CORS na svim rutama
+app.use(express.json()); //automatski dekodiraj JSON poruke
 const port = 3000;
 
-app.get("/storage", (req, res) => {
-  let query = req.query;
-  console.log(query.data);
-  res.json(storage[query.data]);
+app.get("/storage", async (req, res) => {
+  let query = String(req.query.data);
+  console.log(query);
+  let db = await connect();
+  let cursor = await db.collection(query).find();
+  let results = await cursor.toArray();
+  res.json(results);
 });
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
