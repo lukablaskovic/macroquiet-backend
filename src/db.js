218920124a0e4d865488e61aaf1e5df15c1bea5d
lukablaskovic <lblaskovi@unipu.hr>
2022-07-26 16:51:59 +0200

    switched from production to development mode

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 const { MongoClient } = require("mongodb");
 
-let connection_string = process.env.CONNECTION_STRING;
+let connection_string =
+  process.env.CONNECTION_STRING ||
+  "mongodb+srv://admin:6VKpRNUZ2cvEY9xK@cluster0.bhxeo.mongodb.net/stranded-away";
 // Create a new MongoClient
 const client = new MongoClient(connection_string);
 let db = null;
