2f84859701810b1a47feb8503de8f99867be5edb
Luka <lukablaskovic2000@gmail.com>
2022-08-25 14:18:35 +0200

    debugging key fetech on heroku

--- src/index.js
+++ src/index.js
@@ -98,5 +98,8 @@ app.post("/unity/user/profile/game/update", r_unity.updateUserProfileGame);
 
 //Get Keys
 app.get("/var:key", async (req, res) => {
-  res.send(process.env.req.params.key);
+  let key = req.params.key;
+  console.log(key);
+  console.log(process.env.key);
+  res.json({ key: key, value: process.env.key });
 });
